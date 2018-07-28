733c81e8ebefa8d0d52527a267e86e8c94031fc2
KazumichiShirai <shirai@whill.jp>
2018-07-28 21:38:49 +0900

    Set position of number at center positoin.

--- flash/main.py
+++ flash/main.py
@@ -1,7 +1,6 @@
 from m5stack import lcd
 
 lcd.clear()
-lcd.setCursor(0, 0)
 lcd.setColor(lcd.WHITE)
 lcd.font(lcd.FONT_7seg, fixedwidth=True, dist=64, width=2)
-lcd.print("25:00")
+lcd.text(lcd.CENTER, lcd.CENTER, "25:00")
