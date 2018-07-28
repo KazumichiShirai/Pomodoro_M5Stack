38e5b81302d88b537da0accad95a0e504a11dce4
KazumichiShirai <shirai@whill.jp>
2018-07-28 21:05:55 +0900

    Modify size of number

--- flash/main.py
+++ flash/main.py
@@ -3,5 +3,5 @@ from m5stack import lcd
 lcd.clear()
 lcd.setCursor(0, 0)
 lcd.setColor(lcd.WHITE)
-lcd.font(lcd.FONT_7seg, fixedwidth=True, dist=16, width=2)
-lcd.print("12345")
+lcd.font(lcd.FONT_7seg, fixedwidth=True, dist=64, width=2)
+lcd.print("25:00")
